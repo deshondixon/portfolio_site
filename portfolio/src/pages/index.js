0945a6acce0ec040949fe6aa587d7ed824a64f00
DeShon Dixon <deshondixon@gmail.com>
2023-02-28 02:05:51 -0800

    init

---
+++ portfolio/src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import { motion } from 'framer-motion';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>DeShon Dixon Portfolio Site</title>
+      </Head>
+
+      <h1 className='text-3xl font-bold underline'>DeShon Dixon</h1>
+    </>
+  );
+}
