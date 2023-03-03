a062caf050e4dead9b487f4ba1b7a04ee8400f2e
DeShon Dixon <deshondixon@gmail.com>
2023-03-03 00:00:29 -0800

    acp

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,34 @@
-import { Link } from '@nextui-org/react';
+import { Grid, Container, Link, Text } from '@nextui-org/react';
 
 export default function Navbar() {
   return (
-    <div className='Navbar'>
-      <Link href='/'>About</Link>
-      <Link href='/cart'>Contact Me</Link>
-    </div>
+    <Grid.Container gap={2} justify='center'>
+      <Link href='/'>
+        <Grid xs={12} md={6}>
+          <Text
+            size={30}
+            css={{
+              textGradient: '45deg, $blue600 -20%, $pink600 50%',
+            }}
+            weight='bold'
+          >
+            About
+          </Text>
+        </Grid>
+      </Link>
+      <Link href='/cart'>
+        <Grid xs={12} md={12}>
+          <Text
+            size={30}
+            css={{
+              textGradient: '45deg, $blue600 -20%, $pink600 50%',
+            }}
+            weight='bold'
+          >
+            Contact Me
+          </Text>
+        </Grid>
+      </Link>
+    </Grid.Container>
   );
 }
