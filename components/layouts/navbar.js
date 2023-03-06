885cf1d767c16b5a8ac925d191aff589fbb6bad5
DeShon Dixon <deshondixon@gmail.com>
2023-03-05 22:53:42 -0800

    acp

---
+++ components/layouts/navbar.js
@@ -0,0 +1,16 @@
+import Logo from 'logo.js'
+import Link from '@nextui-org/react'
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
