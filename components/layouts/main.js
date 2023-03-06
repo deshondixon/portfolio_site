a7c7fdccaa98785fcec463912b53912c5f19642b
DeShon Dixon <deshondixon@gmail.com>
2023-03-05 21:01:54 -0800

    acp

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -5,7 +5,13 @@ const Main = ({ children, router }) => {
   return (
     <div>
       <Box as="main" pb={8}>
-        <Head></Head>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <Title>DeShon Dixon - Homepage</Title>
+        </Head>
       </Box>
     </div>
   )
