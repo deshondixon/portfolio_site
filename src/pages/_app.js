c13bcb9a1c660ad89abbe7446a62c785a65b322c
DeShon Dixon <deshondixon@gmail.com>
2023-03-05 21:13:57 -0800

    acp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '../components/layout/main'
+import Layout from 'components/layouts/main'
 
-const Website = ({ Component, pageProps }) => {
+const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider>
       <Layout router={router}>
