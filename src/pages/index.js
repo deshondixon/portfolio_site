9acbbb1db384e2a41b02399f097bb547105b5874
DeShon Dixon <deshondixon@gmail.com>
2023-03-05 22:20:44 -0800

    acp

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const Page = () => {
             weight="bold"
             h1
             css={{
-              textGradient: '45deg, $yellow600 -0%, $red600 100%'
+              textGradient: '45deg, $gray700 -0%, $purple400 40%, $black 100%'
             }}
             size={30}
           >
