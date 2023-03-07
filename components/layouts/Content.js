f190e57872a3b954f130e9c5c581cde29870c3a5
DeShon Dixon <deshondixon@gmail.com>
2023-03-07 15:14:02 -0800

    acp

--- components/layouts/Content.js
+++ components/layouts/Content.js
@@ -1,41 +1,53 @@
-import { Box, Container, Heading } from '@chakra-ui/react'
-import { Avatar, Grid, Card, Text } from '@nextui-org/react'
+import {
+  Container,
+  Avatar,
+  Grid,
+  Card,
+  Text,
+  Spacer,
+  Row,
+  Col
+} from '@nextui-org/react'
 
 export const Content = () => (
   <Container>
-    <Grid.Container gap={2} justify="center">
-      <Grid>
-        <Avatar
-          zoomed
-          squared
-          src="deshon.png"
-          css={{ size: '$36' }}
-          justify="space-between"
-        />
-        <Text
-          h1
-          size={30}
-          css={{
-            textGradient: '45deg, $blue600 -20%, $purple600 50%'
-          }}
-          weight="bold"
-        >
-          DeShon Dixon
-        </Text>
-        <p>( Software Developer )</p>
-      </Grid>
-      <Grid xs={6}>
-        <Text
-          h1
-          size={30}
-          css={{
-            textGradient: '45deg, $blue600 -20%, $purple600 50%'
-          }}
-          weight="bold"
-        >
-          Hello, I&apos;m a Software developer based in Washington!
-        </Text>
-      </Grid>
+    <Grid.Container gap={2}>
+      <Row gap={1}>
+        <Col>
+          <Grid xs={12}>
+            <Text
+              h1
+              size={30}
+              css={{
+                textGradient: '45deg, $blue600 -20%, $purple600 50%'
+              }}
+              weight="bold"
+            >
+              Hello, I&apos;m a Software developer based in Washington!
+            </Text>
+            <Grid xs={12}>
+              <Avatar
+                zoomed
+                squared
+                src="deshon.png"
+                css={{ size: '$36' }}
+                justify="space-between"
+              />
+              <Text
+                h1
+                size={30}
+                css={{
+                  textGradient: '45deg, $blue600 -20%, $purple600 50%'
+                }}
+                weight="bold"
+              >
+                DeShon Dixon
+              </Text>
+              <p>( Software Developer )</p>
+            </Grid>
+          </Grid>
+        </Col>
+      </Row>
     </Grid.Container>
   </Container>
 )
