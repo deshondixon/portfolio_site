a133d0477bb767d9e57ec140d1cbe6e379cfb0cd
DeShon Dixon <deshondixon@gmail.com>
2023-03-07 15:26:19 -0800

    acp

--- components/layouts/Content.js
+++ components/layouts/Content.js
@@ -12,39 +12,36 @@ import {
 export const Content = () => (
   <Container>
     <Grid.Container gap={2}>
-      <Row gap={1}>
-        <Col>
-          <Grid xs={12}>
-            <Text
-              h1
-              size={30}
-              css={{
-                textGradient: '45deg, $blue600 -20%, $purple600 50%'
-              }}
-              weight="bold"
-            >
-              Hello, I&apos;m a Software developer based in Washington!
-            </Text>
-            <Grid xs={12}>
-              <Avatar zoomed squared src="deshon.png" css={{ size: '$36' }} />
-              <Spacer />
-              <Text
-                h1
-                size={30}
-                css={{
-                  textGradient: '45deg, $blue600 -20%, $purple600 50%'
-                }}
-                weight="bold"
-              >
-                DeShon Dixon
-              </Text>
-              <Text size={15} weight="bold">
-                ( Software Developer )
-              </Text>
-            </Grid>
-          </Grid>
-        </Col>
-      </Row>
+      <Col gap={1}>
+        <Grid xs={12}>
+          <Text
+            h1
+            size={30}
+            css={{
+              textGradient: '45deg, $blue600 -20%, $purple600 50%'
+            }}
+            weight="bold"
+          >
+            Hello, I&apos;m a Software developer based in Washington!
+          </Text>
+          <Spacer />
+          <Avatar zoomed squared src="deshon.png" css={{ size: '$36' }} />
+          <Spacer />
+          <Text
+            h1
+            size={30}
+            css={{
+              textGradient: '45deg, $blue600 -20%, $purple600 50%'
+            }}
+            weight="bold"
+          >
+            DeShon Dixon
+          </Text>
+          <Text size={15} weight="bold">
+            ( Software Developer )
+          </Text>
+        </Grid>
+      </Col>
     </Grid.Container>
   </Container>
 )
