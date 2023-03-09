6a1d1fd15a5fed586fe6450eab7ed8be370a98d3
DeShon Dixon <deshondixon@gmail.com>
2023-03-09 13:32:36 -0800

    acp

--- components/layouts/navbar.js
+++ components/layouts/navbar.js
@@ -5,6 +5,7 @@ import { Grid, Switch } from '@nextui-org/react'
 import { SunIcon } from './SunIcon'
 import { MoonIcon } from './MoonIcon'
 import { useColorMode } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function App() {
   const collapseItems = [
@@ -17,60 +18,55 @@ export default function App() {
     'Contact'
   ]
 
-  const ThemeToggleButton = () => {
-    const { toggleColorMode } = useColorMode()
-
-    return (
-      <Layout>
-        <Navbar isBordered variant="floating">
-          <Navbar.Brand>
-            <Navbar.Toggle color="gradient" aria-label="toggle navigation" />
-            <Logo />
-            <Grid.Container gap={2}>
-              <Grid>
-                <Switch
-                  onClick={{ toggleColorMode }}
-                  color="secondary"
-                  shadow
-                  checked={true}
-                  size="xl"
-                  iconOn={<SunIcon filled />}
-                  iconOff={<MoonIcon filled />}
-                />
-              </Grid>
-            </Grid.Container>
-          </Navbar.Brand>
-          <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
-            <Navbar.Link href="#">About</Navbar.Link>
-            <Navbar.Link isActive href="#">
-              Projects
-            </Navbar.Link>
-            <Navbar.Link href="#"> Experience </Navbar.Link>
-          </Navbar.Content>
-          <Navbar.Content>
-            <Navbar.Item>
-              <Button color="gradient" auto ghost as={Link} href="#">
-                Contact
-              </Button>
-            </Navbar.Item>
-          </Navbar.Content>
-          <Navbar.Collapse>
-            {collapseItems.map((item, index) => (
-              <Navbar.CollapseItem key={item}>
-                <Link
-                  color="inherit"
-                  css={{
-                    minWidth: '100%'
-                  }}
-                  href="#"
-                >
-                  {item}
-                </Link>
-              </Navbar.CollapseItem>
-            ))}
-          </Navbar.Collapse>
-        </Navbar>
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <Navbar isBordered variant="floating">
+        <Navbar.Brand>
+          <Navbar.Toggle color="gradient" aria-label="toggle navigation" />
+          <Logo />
+          <Grid.Container gap={2}>
+            <Grid>
+              <Switch
+                color="secondary"
+                shadow
+                checked={true}
+                size="xl"
+                iconOn={<SunIcon filled />}
+                iconOff={<MoonIcon filled />}
+              />
+            </Grid>
+          </Grid.Container>
+        </Navbar.Brand>
+        <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
+          <Navbar.Link href="#">About</Navbar.Link>
+          <Navbar.Link isActive href="#">
+            Projects
+          </Navbar.Link>
+          <Navbar.Link href="#"> Experience </Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Item>
+            <Button color="gradient" auto ghost as={Link} href="#">
+              Contact
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+        <Navbar.Collapse>
+          {collapseItems.map((item, index) => (
+            <Navbar.CollapseItem key={item}>
+              <Link
+                color="inherit"
+                css={{
+                  minWidth: '100%'
+                }}
+                href="#"
+              >
+                {item}
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
+        </Navbar.Collapse>
+      </Navbar>
+    </Layout>
+  )
 }
